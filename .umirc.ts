b9406dab34aa1be875dfbc26ab7c1dee8dd9fdc6
yehuozhili <yehuozhili@outlook.com>
2020-09-21 19:05:03 +0800

    change config

--- .umirc.ts
+++ .umirc.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "dumi";
 
 export default defineConfig({
 	title: "开发指导文档",
-	favicon:
-		"http://www.zhufengpeixun.cn/files/system/2016/02-02/124619b1ad4a134241.png",
-	logo:
-		"http://www.zhufengpeixun.cn/files/system/2016/02-02/124619b1ad4a134241.png",
+	base: "/component-library-guidedoc",
+	publicPath: "/component-library-guidedoc/",
+	exportStatic: {},
+	favicon: "/logo.png",
+	logo: "/logo.png",
 	outputPath: "docs-dist",
 	// more config: https://d.umijs.org/config
 });
